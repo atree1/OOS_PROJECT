ce6cf7e517070160ccb2b7a64ea16879ee7a3e1c
qltqj9793 <rnrmf9793@gmail.com>
2018-12-18 18:27:33 +0900

    dfdf

--- oos/oos-user-service/src/main/java/org/oos/controller/UserController.java
+++ oos/oos-user-service/src/main/java/org/oos/controller/UserController.java
@@ -210,7 +210,7 @@ public class UserController {
 		if(cri.getCategory() != null && cri.getCategory().equals("select2")) {
         	model.addAttribute("store", store);
         }else{
-        	model.addAttribute("product", productService.getMainList(map));
+        	model.addAttribute("product", productService.getList(map));
         }
 		
 		List<Integer> pageList = new ArrayList<>();
