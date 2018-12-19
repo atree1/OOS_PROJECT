aa3daa5e6486d15b6e4209003dbc2e9d4b4c6835
JOSEONGIK <JOSEONGIK@HB03-04>
2018-12-19 16:25:57 +0900

    fg

--- oos/oos-common/src/main/java/org/oos/service/ReplyService.java
+++ oos/oos-common/src/main/java/org/oos/service/ReplyService.java
@@ -9,12 +9,20 @@ import org.oos.domain.ReplyVO;
 public interface ReplyService {
 	
 	public int register(ReplyVO vo);
+	
 	public ReplyVO get(Long rno);
+	
 	public int modify(ReplyVO vo);
+	
 	public int remove(Long rno);
+	
 	public List<ReplyVO> getList(Map<String, Object> map);	
+	
 	public List<ReplyVO> getDetailList(Map<String, Object> map);
+	
 	public ReplyPageDTO getListPage(Map<String, Object> map);
+	
 	public int count(Map<String, Object> map);
+	
 	public int myOrderCount(Map<String, Object> map);
 }
