7a11c8cb2b4246c76b3de09bec0923e3519d38ba
JOSEONGIK <JOSEONGIK@HB03-04>
2018-12-19 12:12:22 +0900

    dd

--- oos/oos-seller-service/src/main/java/org/oos/controller/NotifyController.java
+++ oos/oos-seller-service/src/main/java/org/oos/controller/NotifyController.java
@@ -69,12 +69,11 @@ public class NotifyController {
 		
 	
 	@GetMapping("/sellerRegister")
-<<<<<<< HEAD
-	public void insert(Model model) {
-		
-=======
+
+
+
 	public void insert(String sid, Model model) {
->>>>>>> branch 'master' of https://github.com/atree1/OOS_PROJECT.git
+
 		
   //  	model.addAttribute("sid", service.get(sbno).);
 		
