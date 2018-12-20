e31a93e32874a997cb3a0bf099451f4c3c222318
qltqj9793 <rnrmf9793@gmail.com>
2018-12-20 15:06:21 +0900

    sdfsdf

--- oos/oos-user-service/src/main/resources/static/js/menuhover.js
+++ oos/oos-user-service/src/main/resources/static/js/menuhover.js
@@ -1,26 +1,15 @@
 
 
-	$("#searchOptionSelect").click(function(){
-		if($(".layoutSelectBox").hasClass("open")){
-			slideUp();
-		}else{
-			slideDown();
-		}
-	});    
-	
-	function slideUp(){
-		$(".MultipleSelectBox").slideUp();
-		$(".layoutSelectBox").removeClass("open");
-		$("#searchOptionSelect").addClass("open");
+$("#searchOptionSelect").click(function(){
+	if($(".layoutSelectBox").hasClass("open")){
+		slideUp();
+	}else{
+		slideDown();
 	}
-	
-	function slideDown(){
-		$(".MultipleSelectBox").slideDown();
-		$(".layoutSelectBox").addClass("open");
-		$("#searchOptionSelect").removeClass("open");
-	}
-	
-$(".categoryMenu").hover(
+});    
+
+
+$(".Top_Menu").hover(
         function () {
             $(this).show();
         },
@@ -28,47 +17,15 @@ $(".categoryMenu").hover(
             $(this).hide();
         }
     );
-	
-/*$("#buildingSinsang").hover(
-        function () {
-        	$('.buildingMenu').show();
-        }
-  );*/
 
-/*    function categoryMenuChange(num){
-        $(". categoryMenu").hide();
-        $(".selectCategory").removeClass('active');
 
-        if(num == 1){
-        	$('.buildingMenu').show();
-        }else if(num == 2){
-            $(".manCategoryArea").show();
-        }else if(num == 3){
-            $(".childCategoryArea").show();
-        }
-        $(this).addClass('active');
+function categoryMenuShow(num){
+
+    if(num == 1){
+        $(".Top_Menu").show();
+    }else if(num == 2){
+        $(".Bottom_Menu").show();
     }
 
-
-    
-    function categoryChange(num){
-        $(".selectCategoryArea").removeClass('active');
-        if(num == 1){
-            $(".womanCategoryArea").addClass('active');
-        }else if(num == 2){
-            $(".manCategoryArea").addClass('active');
-        }else if(num == 3){
-            $(".childCategoryArea").addClass('active');
-        }
-
-    }
-*/
-    function categoryMenuShow(num){
-    	$(".categoryMenu").show();
-        if(num == 1){
-        	$('.Top_Menu').show();
-        }else if(num == 2){
-            $(".Top_Menu").show();
-        }
-
-    }
+}
+  
