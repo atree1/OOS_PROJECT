8eb48f94bf77e9328b11058baa5026d882c4fd27
qltqj9793 <rnrmf9793@gmail.com>
2018-12-24 10:17:12 +0900

    sdf

--- oos/oos-user-service/src/main/resources/static/js/function.js
+++ oos/oos-user-service/src/main/resources/static/js/function.js
@@ -75,22 +75,6 @@ $(".fixedZzim").click(function(){
 	actionForm.attr("action", "/user/myStoreList").submit();
 });
 
-function categoryClickFunc(cat){
-	
-	var str = "";
-	var category;
-	if($(".active").hasClass("womanCategory")){
-		category = "women";
-	}else if($(".active").hasClass("manCategory")){
-		category = "men";
-	}else if($(".active").hasClass("childCategory")){
-		category = "children";
-	}
-	
-	str += "<input type='hidden' name='category' value='"+category+"'/>";
-	str += "<input type='hidden' name='subCategory' value='"+cat+"'/>";
-	actionForm.append(str).attr("action","/user/list").submit();
-}
 
 function onlyNumber(event){
     event = event || window.event;
