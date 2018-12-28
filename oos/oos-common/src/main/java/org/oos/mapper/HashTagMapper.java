bd052ad3594e857af175de4ae49338f7c45946cb
qltqj9793 <rnrmf9793@gmail.com>
2018-12-28 11:31:51 +0900

    sdf

--- oos/oos-common/src/main/java/org/oos/mapper/HashTagMapper.java
+++ oos/oos-common/src/main/java/org/oos/mapper/HashTagMapper.java
@@ -3,6 +3,7 @@ package org.oos.mapper;
 import java.util.List;
 
 import org.oos.domain.HashTagVO;
+import org.oos.domain.StoreHashTagVO;
 
 
 
@@ -17,4 +18,10 @@ public interface HashTagMapper {
 	public int insert(String tag);
 	
 	public int delete(int hno);
+	
+	public List<StoreHashTagVO> getStoreTagList(Long sno);
+	
+	public void insertStore(StoreHashTagVO vo);
+	
+	public int deleteAll(Long sno);
 }
